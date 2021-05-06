5aa3847b95e2448a3eee809617f8901eb8812b09
Maria <imgvoid@gmail.com>
2021-05-06 14:31:58 +0300

    new exercises has been added

---
+++ PythonBasic/files/count_file_lines_answer.py
@@ -0,0 +1,34 @@
+"""
+Напишите программу, которая открывает текстовый файл и выводит на экран
+каждую строчку текста по очереди, подписывая её порядковым номером.
+Затем выведите на экран количество линий в тексте.
+
+"""
+
+
+def file_lengthy(path):
+    """Функция со счетчиком"""
+    print('1)', file_lengthy.__doc__, end=':\n\n')
+    with open(path, 'r') as f:
+        count = 0
+        for line in f.readlines():
+            count += 1
+            print(f'{count} line:\n{line}', end='')
+        return count
+
+
+print("\nNumber of lines in the file:", file_lengthy("data/file.txt"), end='\n\n')
+
+
+def file_lengthy(path):
+    """Функция с использованием enumerate(list)"""
+    print('2)', file_lengthy.__doc__, end=':\n\n')
+    with open(path, 'r') as f:
+        length = 0
+        for index, line in enumerate(f.readlines()):
+            length = index + 1
+            print(f'{index + 1} line:\n{line}', end='')
+        return length
+
+
+print("\nNumber of lines in the file:", file_lengthy("data/file.txt"), end='\n\n')
